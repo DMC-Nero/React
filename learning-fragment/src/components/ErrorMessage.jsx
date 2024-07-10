e8f35f91be3bf8386082c9b107b7d70dedb088cf
DMC-Nero <vemulanatraj0123@gmail.com>
2024-07-10 23:57:56 +0530

    11:11

--- learning-fragment/src/components/ErrorMessage.jsx
+++
@@ -1,8 +0,0 @@
-import styles from "./ErrorMessage.module.css";
-
-const ErrorMessage = ({items}) => {
-
-    return <>{ items.length === 0 && <h3 className={ styles["pk-h3-style"] }>I am still hungry.</h3> } </>;
-  };
-  export default ErrorMessage;
-  
